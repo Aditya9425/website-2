0836aff4362d3d2a89dc9964559da8929e6124c6
Aditya9425 <adityabathla9@gmail.com>
2025-10-08 16:03:46 +0530

    maintenance mode disabled

--- maintenance-check.js
+++ maintenance-check.js
@@ -1,5 +1,5 @@
 // Maintenance mode toggle - set to true to enable maintenance mode
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 
 // Check maintenance mode and redirect if needed
 if (MAINTENANCE_MODE && !window.location.pathname.includes('maintenance.html')) {
